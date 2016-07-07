85a339eeca678541258e59b00ced8ea28a4738fe
Guillaume MARSILLE <guillaume.marsille@prestashop.com>
2016-07-07 16:46:23 +0200

    trying connection to saucelabs

--- test/itg/1.7/common.webdriverio.js
+++ test/itg/1.7/common.webdriverio.js
@@ -8,7 +8,7 @@ var options = {
     waitForTimeout: 30000,
     desiredCapabilities: {
         browserName: 'chrome',
-		caps: true
+		'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
 	},
 	port: 4445
 };

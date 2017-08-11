d6f61ee36ec035454f1eed4c93253be4186d292a
nesrine abdmouleh <nesrine.abdmouleh@sifast.com>
2017-08-11 11:14:41 +0100

    fix errors in check_order & create_order

--- test/itg/1.7/scenario/BO/Order/create_order.webdriverio.js
+++ test/itg/1.7/scenario/BO/Order/create_order.webdriverio.js
@@ -1,7 +1,7 @@
 'use strict';
 var should = require('should');
-var common = require('../../common.webdriverio');
-var globals = require('../../globals.webdriverio.js');
+var common = require('../../../common.webdriverio');
+var globals = require('../../../globals.webdriverio.js');
 
 describe('Check the order in BO', function () {
     common.initMocha.call(this);
